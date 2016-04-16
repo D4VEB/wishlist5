2d5d4586761d6095a26dd5e6df7de30f9264097c
David <dhblodgett@gmail.com>
2016-04-15 22:51:04 -0700

    f

--- wishlists/seralizers.py
+++ wishlists/seralizers.py
@@ -7,11 +7,11 @@ class UserSerializer(serializers.ModelSerializer):
     items = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     lists = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     profile = serializers.PrimaryKeyRelatedField(read_only=True)
-
+    password = serializers.CharField(max_length=128, write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'lists', 'items', 'profile')
+        fields = ('id', 'username', 'password', 'lists', 'items', 'profile')
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
